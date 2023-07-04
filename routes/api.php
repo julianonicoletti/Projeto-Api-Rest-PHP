9d53b874fb9a75b27510aa07dcf1d472f418c764
Julianonicoletti <juliano.nicoletti@edu.unifil.br>
2023-07-04 16:08:32 -0300

    alterando as rotas e o readme

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\tasksController;
 | be assigned to the "api" middleware group. Make something great!
 |
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 */
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
